23b5938ef09b0e9314bd6d26797feb2237f1833b
Your Name <you@example.com>
2023-08-07 04:04:49 +0900

    create placeData & create midreware

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\v1\PlaceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('verifyFirebaseToken')->group(function (){
-    Route::prefix('api/vi')->group(function (){
-    });
+Route::middleware('verifyFirebaseToken')->prefix('v1')->group(function (){
+    Route::post('/place', [PlaceController::class, 'store']);
 });
