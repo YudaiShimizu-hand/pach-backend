65d9c12125fe5d98c174035403ad7c19249c4f27
Your Name <you@example.com>
2023-08-14 18:52:41 +0900

    get analysisi app

--- src/app/Http/Controllers/v1/DataController.php
+++ src/app/Http/Controllers/v1/DataController.php
@@ -37,4 +37,19 @@ class DataController extends Controller
         $data->save();
         return response()->json(['message' => '記録情報の登録に成功しました。']);
      }
+
+     public function analysis(Request $request)
+     {
+        $place_id = Place::where('place_name', $request->place_name)->where('firebase_user_id', $request->get('firebase_user_id'))->value('id');
+        $shop_id = Shop::where('shop_name', $request->shop_name)->where('firebase_user_id', $request->get('firebase_user_id'))->value('id');
+        $machine_id = Machine::where('machine_name', $request->machine_name)->where('firebase_user_id', $request->get('firebase_user_id'))->value('id');
+        $getAnalysis = [];
+        $getAnalysis = $this->where('place_id', $place_id)->where('shop_id', $shop_id)->where('machine_id', $machine_id);
+        $totalData = count($getAnalysis);
+        $getWinData = [];
+        $getWinData = $getAnalysis->where('judge_flag', true);
+        $winData = count(getWinData);
+        $analysisData = $winData / $getWinData * 100;
+        return response()->json($analysisData, 200);
+     }
 }
